6e1eaaf10d0af861a6ecfcb33d6df7692b1475f4
shawon ahmmed <shawonahmmed08509@gmail.com>
2021-11-01 17:02:15 +0600

    page not found page created

--- src/Components/PageNotFound/PageNotFound.js
+++ src/Components/PageNotFound/PageNotFound.js
@@ -3,7 +3,9 @@ import React from 'react';
 const PageNotFound = () => {
     return (
         <div>
-            <h1>page not found</h1>
+            <section>
+                <h1 className="text-center">The page you are searching for is not found!!</h1>
+            </section>
         </div>
     );
 };
